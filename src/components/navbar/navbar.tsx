72a4df22998b7ffa4380fe546aad3a9db7ad7f7a
Emerson Shatouhy <emersont2003@gmail.com>
2025-03-21 14:40:05 -0400

    push

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -50,14 +50,14 @@ export function Navbar() {
                     </nav>
                 </div>
 
-                <Button size="sm" className="h-8"
+
+                {/* <Button size="sm" className="h-8"
                     onClick={handleClick}>
                     <Plus className="mr-2 h-4 w-4" />
                     New Test
-                </Button>
-
+                </Button> */}
             </div>
-
+            <button onClick={() => alert("HIT")}>Click Me</button>
 
         </header>
     )
