301330f019ae8974a99fc58ea304d498d1e7772e
Emerson Shatouhy <emersont2003@gmail.com>
2025-03-21 23:47:16 -0400

    Cleaned up button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { createClient } from '@/utils/supabase/client'
+import Image from 'next/image'
 
 export default function LoginPage() {
     const supabase = createClient();
@@ -14,7 +15,12 @@ export default function LoginPage() {
         })
     }
     return (
-        <Button onClick={signInWithAzure}>Sign in with Azure</Button>
+        <div className='flex flex-col items-center h-screen p-4'>
+            <Button onClick={signInWithAzure} className='text-xl rounded-none p-6'>
+                <Image src="/ms-logo.svg" alt="ErgScoreManager" width={25} height={25} />
+                Sign in with Microsoft</Button>
+        </div>
+
 
     )
 }
