7caeefdfd5732eb4e54fa715b83fb2168d1a4bd3
Emerson Shatouhy <emersont2003@gmail.com>
2025-04-04 14:27:14 -0400

    Fixed navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -28,10 +28,65 @@ export async function Navbar() {
     const user = await checkUserClient()
     const athleteResponse = await getSignedInAthlete()
     if ('error' in athleteResponse) {
-        console.error('Failed to fetch athlete data:', athleteResponse.error)
-        return null
+        console.warn('Not signed in')
+        return (
+            <header className="flex p-4 border-b border-red-900 w-full bg-red-800 opacity-89 text-white">
+                <div className="flex flex-row justify-between w-full" >
+                    <div className="flex items-center space-x-4">
+                        <Link href="/" className="font-bold text-lg">
+                            WPI Rowing Dashboard
+                        </Link>
+                        <nav className="flex items-center space-x-4">
+                            {navItems.map((item) => (
+                                <Link
+                                    key={item.href}
+                                    href={item.href}
+                                    className={cn(
+                                        "flex items-center transition-colors hover:text-foreground/80",
+
+                                    )}
+                                >
+                                    <item.icon className="mr-2 h-4 w-4" />
+                                    {item.name}
+                                </Link>
+                            ))}
+                        </nav>
+                    </div>
+                    <div className="flex items-center space-x-4">
+                        {/* {'user' in user && (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>{user.user.user_metadata.full_name}</DropdownMenuTrigger>
+                            <DropdownMenuContent>
+
+                                {athleteData.coxswain && (
+                                    <>
+                                        <DropdownMenuItem>
+                                            <a href="/manage-test">Manage Test</a>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <a href="/roster">Manage Rosters</a>
+                                        </DropdownMenuItem>
+                                    </>
+                                ) || (
+                                        <DropdownMenuItem>
+                                            <a href="/my-scores">My Scores</a>
+                                        </DropdownMenuItem>
+                                    )}
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <a href="/signout">Sign Out</a>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    )} */}
+
+                    </div>
+                </div>
+            </header>
+        )
     }
     const athleteData: Athlete = athleteResponse
+
 
     if ('user' in user) {
 
@@ -80,7 +135,7 @@ export async function Navbar() {
                                     </>
                                 ) || (
                                         <DropdownMenuItem>
-                                            <a href="/my-scores">My Scores</a>
+                                            <a href="/myScores">My Scores</a>
                                         </DropdownMenuItem>
                                     )}
                                 <DropdownMenuSeparator />
